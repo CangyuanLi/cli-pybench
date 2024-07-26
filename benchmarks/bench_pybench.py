d83cbb10439f399b26ab38f8e04389e53287c3d6
Cangyuan Li <everest229@gmail.com>
2024-07-26 00:30:57 -0400

    tests

--- benchmarks/bench_pybench.py
+++ benchmarks/bench_pybench.py
@@ -2,12 +2,34 @@ import pybench
 
 
 @pybench.config(number=10)
+def bench_my_func4():
+    return 1
+
+
+@pybench.skipif(True)
+@pybench.config(number=10)
+def bench_my_func5():
+    return 1
+
+
+@pybench.config(number=10)
+@pybench.skipif(True)
+def bench_my_func6():
+    return 1
+
+
+@pybench.config(number=10)
 @pybench.parametrize({"a": [1, 2], "b": [3, 4]})
 def bench_my_func(a, b):
     return a * b
 
 
-@pybench.config(number=10)
+@pybench.skipif(False)
 @pybench.parametrize({"a": [1, 2], "b": [3, 4]})
 def bench_my_func2(a, b):
     return a + b
+
+
+@pybench.parametrize({"a": [1, 2]})
+def bench_my_func3(a):
+    return a
